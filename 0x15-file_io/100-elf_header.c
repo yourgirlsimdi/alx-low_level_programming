3d2e2b12581f250c62b36323617d2c3781e0f6be
yourgirlsimdi <chisimdi.eso@gmail.com>
2023-06-14 10:44:20 -0700

    0x15. C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,76 +8,85 @@
 
 int read_elf_header(int fd, elf_header_t *header)
 {
-  ssize_t bytes_read;
+	ssize_t bytes_read;
 
-  /* Read the ELF header */
-  bytes_read = read(fd, header, sizeof(elf_header_t));
-  if (bytes_read == -1)
-    return -1;
+	/* Read the ELF header */
+	bytes_read = read(fd, header, sizeof(elf_header_t));
+	if (bytes_read == -1)
+		return -1;
 
-  return 0;
+	return 0;
 }
 
 void print_elf_header(elf_header_t *header)
 {
-  int i;
+	int i;
 
-  printf("ELF Header:\n");
-  printf("  Magic:   ");
-  for (i = 0; i < EI_NIDENT; i++)
-    printf("%02x ", header->e_ident[i]);
-  printf("\n");
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	printf("\n");
 
-  printf("  Class:                             ");
-  switch (header->e_ident[4])
-  {
-  case 0x01:
-    printf("ELF32\n");
-    break;
-  case 0x02:
-    printf("ELF64\n");
-    break;
-  default:
-    printf("<unknown>\n");
-    break;
-  }
+	printf("  Class:                             ");
+	switch (header->e_ident[4])
+	{
+	case 0x01:
+		printf("ELF32\n");
+		break;
+	case 0x02:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown>\n");
+		break;
+	}
 
-  printf("  Data:                              ");
-  switch (header->e_ident[5])
-  {
-  case 0x01:
-    printf("2's complement, little endian\n");
-    break;
-  case 0x02:
-    printf("2's complement, big endian\n");
-    break;
-  default:
-    printf("<unknown>\n");
-    break;
-  }
+	printf("  Data:                              ");
+	switch (header->e_ident[5])
+	{
+	case 0x01:
+		printf("2's complement, little endian\n");
+		break;
+	case 0x02:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown>\n");
+		break;
+	}
 
-  printf("  Version:                           %d (current)\n", header->e_ident[6]);
+	printf("  Version:                           %d (current)\n", header->e_ident[6]);
 
-  printf("  OS/ABI:                            ");
-  switch (header->e_ident[7])
-  {
-  case 0x00:
-    printf("UNIX - System V\n");
-    break;
-  case 0x01:
-    printf("HP-UX\n");
-    break;
-  case 0x02:
-    printf("NetBSD\n");
-    break;
-  case 0x03:
-    printf("Linux\n");
-    break;
-  case 0x06:
-    printf("Solaris\n");
-    break;
-  case 0x07:
-    printf("AIX\n");
-    break;
- 
+	printf("  OS/ABI:                            ");
+	switch (header->e_ident[7])
+	{
+	case 0x00:
+		printf("UNIX - System V\n");
+		break;
+	case 0x01:
+		printf("HP-UX\n");
+		break;
+	case 0x02:
+		printf("NetBSD\n");
+		break;
+	case 0x03:
+		printf("Linux\n");
+		break;
+	case 0x06:
+		printf("Solaris\n");
+		break;
+	case 0x07:
+		printf("AIX\n");
+		break;
+	default:
+		printf("<unknown>\n");
+		break;
+	}
+}
 
+void print_error(char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(98);
+}
